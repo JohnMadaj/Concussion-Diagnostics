741d1ac7576839e1c6ce033fa4304dd28c1ce026
John Madaj <john.madaj.19@cnu.edu>
2022-11-14 09:31:09 -0500

    Diagnostic.py

---
+++ Diagnostic.py
@@ -0,0 +1,34 @@
+import constants
+import Participant
+import Athlete
+
+
+def areTheyConcussed(LAthreshold, AAthreshold, LA, AA):
+    # code
+
+    """
+    :param LAthreshold:
+    :param AAthreshold:
+    :param LA:
+    :param AA:
+    :return:
+
+    TODO: add sophistication to diagnostic:
+    - create degrees of impact severity that reflect on participant overview:
+        (concept)
+            % of threshold      color       severity
+            85                  red         high
+            60                  orange      medium
+            40                  yellow      moderate
+            <30%                green       normal
+    - WTSC
+    - Multiple impact adjustment
+    - EEG
+    - Cumulative impact over time
+    - blood oxygen bias??
+
+    """
+
+    if LA > LAthreshold or AA>AAthreshold:
+        return True
+    return False
